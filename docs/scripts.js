58bf6430dc231a86dec3c9ffa498dce13753a5ad
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2022-03-29 11:37:21 +0300

    Avaa kaikki ulkoiset linkit uuteen välilehteen

--- docs/scripts.js
+++ docs/scripts.js
@@ -29,3 +29,6 @@ function addVideoPopOutLinks() {
 }
 
 addVideoPopOutLinks();
+
+// Open all external links in new tabs or windows
+document.querySelectorAll('a[href^="http"]').forEach(link => link.target = '_blank');
